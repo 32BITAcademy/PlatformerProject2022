0e933af69c6d19509c0bafba824eba238a45de04
Airen <tim.turaev2017@yandex.ru>
2022-12-19 18:29:55 +0300

    changed sprite

--- OOP_Platformer/Source.cpp
+++ OOP_Platformer/Source.cpp
@@ -10,15 +10,15 @@ int main()
 	RenderWindow window(sf::VideoMode(200, 200), "Lesson 1. kychka-pc.ru");
 	
 	Texture tex;
-	tex.loadFromFile("images\\mc.png");
+	tex.loadFromFile("images\\char_black.png");
 	Sprite s(tex);
-	s.setTextureRect({ 0, 0, 64, 64 });
+	s.setTextureRect({ 0, 0, 128, 128 });
 
 	Clock clock;
 	clock.restart();
 
 	GameManager GM;
-	GM.AddObject(new Player({ 100.f, 100.f }, { 64.f, 64.f }, s));
+	GM.AddObject(new Player({ 100.f, 100.f }, { 128.f, 128.f }, s));
 
 	while (window.isOpen())
 	{
