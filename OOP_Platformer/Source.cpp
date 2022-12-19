a5717722941928e60c77c9b1aa2caf55e237a175
Rokokoshik <keklololo51@gmailcom>
2022-12-19 19:03:50 +0300

    Добавил новый класс платформы, ее спрайт и отображение текстуры

--- OOP_Platformer/Source.cpp
+++ OOP_Platformer/Source.cpp
@@ -2,6 +2,7 @@
 #include"GameObject.h"
 #include"Player.h"
 #include"GameManager.h"
+#include"Platform.h"
 
 using namespace sf;
 
@@ -9,17 +10,19 @@ int main()
 {
 	RenderWindow window(sf::VideoMode(800, 800), "Lesson 1. kychka-pc.ru");
 	
-	Texture tex;
+	Texture tex,tex1;
 	tex.loadFromFile("images\\char_black.png");
 	Sprite s(tex);
 	s.setTextureRect({ 0, 0, 128, 128 });
-
+	tex1.loadFromFile("images\\platform.png");
+	Sprite x(tex1);
+	x.setTextureRect({ 0,404, 800,150 });
 	Clock clock;
 	clock.restart();
 
 	GameManager GM;
+	GM.AddObject(new Platform({ 400.f, 725.f }, { 800.f,150.f }, x));
 	GM.AddObject(new Player({ 400.f, 400.f }, { 128.f, 128.f }, s));
-
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -29,7 +32,7 @@ int main()
 				window.close();
 		}
 
-		float dt = clock.restart().asSeconds()*15;
+		float dt = clock.restart().asSeconds()*4;
 		GM.Update(dt);
 
 		window.clear();
